9ca1bdda451b7ff6983e984e81bc91d9193f2d56
JonasDev99 <jonas.suter@fhnw.ch>
2018-02-04 14:26:26 +0100

    added comment

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    'secret': '4cVPAA9OBQtaJrbPmKBZ',
+    'secret': '4cVPAA9OBQtaJrbPmKBZ', //don't worry, this is not the actual password
     'database': 'mongodb://api.stronghands.info'
 
 };
